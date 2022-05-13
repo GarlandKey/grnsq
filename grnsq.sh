3ac8d516efedae6e978b7edcf875ac82b3a4d55b
Garland Key <garlandkey@protonmail.com>
2022-05-12 23:41:38 -0500

    updated script

--- grnsq.sh
+++ grnsq.sh
@@ -5,20 +5,6 @@ filename=grnsq.log
 str="$count green square(s) added."
 year="2022"
 
-#get_git () {
-#    printf "Enter your GitHub repository URL: "
-#    read url
-#    git clone $url
-#    cd $(basename $url .git)
-#}
-
-get_grnsqr () { # $1 = $str, $2 = $filename, $3 = $year
-    echo $1 >> $2
-    git add .
-    git commit --date="$3-$[$RANDOM % 12 + 1]-$[$RANDOM % 28 + 1]" -m "$1"
-}
-
-# press q to quit
 check_quit () {
     read -s -n 1 -t 0.1 input
     if [[ $input = "q" ]] || [[ $input = "Q" ]]
@@ -28,11 +14,16 @@ check_quit () {
     fi
 }
 
-get_git
+printf "Enter your GitHub repository URL: "
+read url
+git clone $url
+cd $(basename $url .git)
 
 while [ "true" ]
 do
-    get_grnsqr $str $filename $year
+    echo $str >> $filename
+    git add .
+    git commit --date="$year-$[$RANDOM % 12 + 1]-$[$RANDOM % 28 + 1]" -m "$str"
     let "count++"
     check_quit
 done
